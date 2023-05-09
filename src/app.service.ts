a3060b4b358a51134674c292f752e36f045b8522
vparkov <vparkov@gmail.com>
2023-05-09 21:57:05 +0100

    feat: test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('AppService.getHello()');
     return 'Hello World!!!';
-  }
-
-  getHello2(): string {
-    console.log('AppService.getHello2()');
-    return 'Hello World2!!!';
   }
 }
